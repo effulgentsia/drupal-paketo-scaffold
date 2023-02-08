621ba2d8eda13ff2bdf0e5d7d28cbfa788990d33
effulgentsia <alex.bronstein@acquia.com>
2023-02-08 07:41:25 -0800

    Delete settings.php

--- scaffold/web/sites/default/settings.php
+++
@@ -1,7 +0,0 @@
-<?php
-
-$drupal_paketo_scaffold['app-root'] = $_SERVER['CNB_APP_DIR'] ?? '/workspace';
-require $drupal_paketo_scaffold['app-root'] . '/.drupal/drupal-paketo-scaffold.settings.php';
-if (isset($_SERVER['DRUPAL_ADDITIONAL_SETTINGS_FILE'])) {
-  @include $drupal_paketo_scaffold['app-root'] . '/' . $_SERVER['DRUPAL_ADDITIONAL_SETTINGS_FILE'];
-}
